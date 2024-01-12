daad6c847880ac6ec513cd145c1271a92fa5bc0b
Jyoti Manglani <manglanijyoti62@gmail.com>
2024-01-12 20:05:48 +0530

    feat: Implement modular filter components
    - Created Filters component to dynamically render filter items.
    - Introduced FilterItem component for individual filter display.
    - Applied consistent styling to Filters, FilterItem, and Delivery components.
    - Used Filters component in the Delivery view to display delivery-related filters.

--- src/components/delivery/index.jsx
+++ src/components/delivery/index.jsx
@@ -1,8 +1,45 @@
 import React from "react";
 import "./delivery.css";
+import Filters from "../filters";
+
+const deliveryFilters = [
+  {
+    id: 1,
+    icon: <i class="fa-solid fa-sliders absolute-center"></i>,
+    title: "Filters",
+  },
+  {
+    id: 2,
+    title: "Rating: 4.0+",
+  },
+  {
+    id: 3,
+    title: "Safe and Hygienic",
+  },
+  {
+    id: 4,
+    title: "Pure Veg",
+  },
+  {
+    id: 5,
+    title: "Delivery Time",
+    icon: <i class="fa-solid fa-arrows-up-down absolute-center"></i>,
+  },
+
+  {
+    id: 6,
+    title: "Great Offers",
+  },
+];
 
 const Delivery = () => {
-  return <div>Delivery</div>;
+  return (
+    <div>
+      <div className="max-width">
+        <Filters filterList={deliveryFilters} />
+      </div>
+    </div>
+  );
 };
 
 export default Delivery;
